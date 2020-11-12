e0173664198f6e5f04caa6f43bedd1e46f4745b0
Jeff Bond <jeffbond@MacBook-Pro.local>
2020-11-12 09:19:16 -0500

    save before drastic changes

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const DOMAIN = 'https://www.theaudiodb.com';
-//const API_KEY = 'https://www.theaudiodb.com'
-//const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
-const getOptions = async (title) => {
-  const url = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artists}` //where we get the data f
-
+const API_KEY = 'https://www.theaudiodb.com/api/v1/json/1/search.php?';
+const BASE_URL = `${DOMAIN}`;
+const getOptions = async (artist) => {
+  const url = 'https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artists}';
 
         try {
           const response = await axios.get(url)
           const artistList = response.data.Search
-          console.log(artistList)
-          // const list = Object.keys(response.data.message)
+          //console.log(artistList)
+          //const list = Object.keys(response.data.message)
            optionValues(artistList)
 
         } catch (error) {
-          console.log(error)
+          //console.log(error)
         }
 }
 const renderList = (artistArray) => {
@@ -21,19 +20,19 @@ const renderList = (artistArray) => {
     let artistInfo = `
     <a href="${BASE_URL}i=${artists.strArtist}">${artist.strArtist}</a>
   `;
-    const listContainer = document.querySelector('.artist-list');
+    const listContainer = document.querySelector('.artist-info');
     listContainer.insertAdjacentHTML('beforeend', artistInfo);
   })
 };
 
 
-function optionValues(list) {
+function optionValues(artistList) {
  
-  const select = document.querySelector('#artistSlide')
+  const select = document.querySelector('#artistData')
   console.log(select)
-  return list.forEach((artist) => {
-    const option = document.createElement('p')
-    option.value = `${artist}`
+  return artistList.forEach((artistList) => {
+    const option = document.createElement('div')
+    //option.value = `${artist}`
     
     option.textContent = `
      ${artists.strArtist} 
